2d4d043b21b7bf47f66ca1a975b90e232062b201
arnavsshah <shah.arnav@yahoo.com>
2022-03-26 03:00:18 +0530

    [Add] requirements.txt

--- server/image_captioning/index.js
+++ server/image_captioning/index.js
@@ -17,11 +17,11 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 //     .then(response => response.json())
 //     .then(data => console.log(data));
 
-// const url = encodeURIComponent("https://images.unsplash.com/photo-1587405254461-abd1d1c7440e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
-// // const url = "hey";
-// fetch(`http://127.0.0.1:5000/img_captioning?img_url=${url}`)
-//     .then(response => response.json())
-//     .then(data => console.log(data));
+const url = encodeURIComponent("https://images.unsplash.com/photo-1587405254461-abd1d1c7440e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+// const url = "hey";
+fetch(`http://127.0.0.1:5000/img_captioning?img_url=${url}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
 
 const text = 'A cat is riding a bicycle'
 fetch(`http://127.0.0.1:5000/get_keywords?text=${text}`)
